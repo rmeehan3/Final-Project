a1e38cd9a2fe74da47186229257dab1a9474f501
rmeehan3 <136287809+rmeehan3@users.noreply.github.com>
2023-07-26 09:38:41 -0400

    Fixed minor error and changed view button design

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from tkinter import ttk
 import sv_ttk
 import sqlite3
 
+# Creating main window
 window = tkinter.Tk()
 window.title("Aux Inventory")
 window.iconbitmap('companylogofavicon.ico')
 window.resizable(width = False, height = False)
-
 frame = tkinter.Frame(window)
 frame.pack()
-
+# Theme
 sv_ttk.set_theme("light")
-
+# DB connection
 connection = sqlite3.connect("inventoryList.db")
 
-#Inventory Display Frame
+#Inventory display frame
 inventory_frame = ttk.LabelFrame(frame, text="Inventory")
 inventory_frame.grid(row=1, column=0, padx=20, pady=25)
 
@@ -30,7 +30,7 @@ inventory_scrollbar = ttk.Scrollbar(inventory_frame, orient=tkinter.VERTICAL, co
 inventory_scrollbar.grid(row=0, column=1, sticky="ns")
 inventory_tree.config(yscrollcommand=inventory_scrollbar.set)
 
-# View Audio Inventory
+# View audio inventory
 def viewAudio():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM Audio")
@@ -43,16 +43,17 @@ def viewAudio():
 
         #Clears existing data from the table
         inventory_tree.delete(*inventory_tree.get_children())
-
         for item in data:
             inventory_tree.insert("", "end", values=item)
     else:
         inventory_frame.grid_forget()
         view_audio_button.config(text="View Inventory")
 
+# Hide inventory on start
 def hideAudioInventory():
     inventory_frame.grid_remove()
 
+# Refresh inventory to display new data
 def refreshInventory():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM Audio")
@@ -63,7 +64,7 @@ def refreshInventory():
     for item in data:
         inventory_tree.insert("", "end", values=item)
 
-# Add Audio Inventory
+# Add audio inventory
 def addNewAudio():
     cursor = connection.cursor()
     AudioID = int(item_id_entry.get())
@@ -72,6 +73,7 @@ def addNewAudio():
     cursor.execute("""INSERT INTO Audio(AudioID, AudioName, AudioAmount) VALUES (?,?,?)""", (AudioID, AudioName, AudioAmount))
     connection.commit()
 
+# Edit audio inventory
 def editAudioAmount():
     cursor = connection.cursor()
     AudioID = int(item_id_entry.get())
@@ -79,7 +81,7 @@ def editAudioAmount():
     cursor.execute("""UPDATE Audio SET AudioAmount = ? WHERE AudioID = ?""", (AudioAmount, AudioID))
     connection.commit()
 
-# Remove Audio Inventory
+# Remove audio inventory
 def removeAudio():
     cursor = connection.cursor()
     del_id = int(item_id_entry.get())
@@ -89,56 +91,58 @@ def removeAudio():
 #Calling the hide function to hide the inventory table when the program is first opened (not sure if this is the best way to do this feel free to change)
 hideAudioInventory()
 
-# Working on this, view inventory function
-view_inv_frame = ttk.LabelFrame(frame, text = "View Inventory")
-view_inv_frame.grid(row = 0, column = 0, padx = 20, pady = 25)
+# View inventory frame and button
+view_inv_frame = ttk.LabelFrame(frame, text = "Inventory")
+view_inv_frame.grid(row = 0, column = 0)
+view_audio_button = ttk.Button(frame, text="View Inventory", command=viewAudio)
+view_audio_button.grid(row=0, column=0, sticky = "news", padx=15, pady=15)
 
-
-view_audio_button = ttk.Button(view_inv_frame, text="View Inventory", command=viewAudio)
-view_audio_button.grid(row=0, column=0, padx=15, pady=15)
-
-#view_audio_button = ttk.Combobox(view_inv_frame, values = ["Audio", "Visual", "Other"])
-#view_audio_button.grid(row = 0, column = 0, padx = 15, pady = 15)
-
-# Add Audio
+# Edit inventory frame
 inventory_audio_frame = ttk.LabelFrame(frame, text = "Inventory")
 inventory_audio_frame.grid(row = 2, column = 0, padx = 20, pady = 20)
-
 item_info_frame = ttk.LabelFrame(frame, text = "Edit Inventory")
 item_info_frame.grid(row = 2, column = 0, padx = 20, pady = 20)
 
-# Product ID
+# Product ID label
 item_id_label = ttk.Label(item_info_frame, text = "Product ID")
 item_id_label.grid(row = 2, column = 0)
+# Get ID
 item_id_entry = ttk.Entry(item_info_frame)
 item_id_entry.grid(row = 3, column = 0, padx = 15, pady = 5)
 
-# Item Name
+# Item name label
 item_name_label = ttk.Label(item_info_frame, text = "Item Name")
 item_name_label.grid(row = 2, column = 1)
+# Get name
 item_name_entry = ttk.Entry(item_info_frame)
 item_name_entry.grid(row = 3, column = 1, padx = 15, pady = 5)
 
-# Item Amount
+# Item amount label
 item_count_label = ttk.Label(item_info_frame, text = "Amount")
 item_count_spinbox = ttk.Spinbox(item_info_frame, from_ = 1, to = 1000)
 item_count_label.grid(row = 2, column = 2)
+# Get amount
 item_count_entry = ttk.Entry(item_info_frame)
 item_count_spinbox.grid(row = 3, column = 2, padx = 15, pady = 5)
 
+# Add audio item button
 add_audio_button = ttk.Button(item_info_frame, text = "Add New Item", command = addNewAudio)
 add_audio_button.grid(row = 4, column = 0, sticky = "news", padx = 15, pady = 10)
 
+# Change audio item amount button
 edit_audio_button = ttk.Button(item_info_frame, text = "Change Amount", command = editAudioAmount)
 edit_audio_button.grid(row = 4, column = 1, sticky = "news", padx = 15, pady = 10)
 
-add_audio_button = ttk.Button(item_info_frame, text = "Delete", command = removeAudio)
-add_audio_button.grid(row = 4, column = 2, sticky = "news", padx = 15, pady = 10)
+# Delete audio item button
+del_audio_button = ttk.Button(item_info_frame, text = "Delete", command = removeAudio)
+del_audio_button.grid(row = 4, column = 2, sticky = "news", padx = 15, pady = 10)
 
 # Refresh button
 refresh_button = ttk.Button(inventory_frame, text="Refresh", command=refreshInventory)
 refresh_button.grid(row=1, column=0, padx=10, pady=5)
 
+# Main loop
 window.mainloop()
 
+# Close DB connection
 connection.close()
