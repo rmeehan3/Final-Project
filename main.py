7bee59f4bf523a4865f8b71a054e93bdc86915b6
Sam <sammo00@comcast.net>
2023-07-25 22:19:46 -0400

    view inventory function implemented into main app

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sqlite3
 
 window = tkinter.Tk()
 window.title("Aux Inventory")
-window.iconbitmap('C:/Users/ryder/python_final_project/companylogofavicon.ico')
+window.iconbitmap('companylogofavicon.ico')
 window.resizable(width = False, height = False)
 
 frame = tkinter.Frame(window)
@@ -15,13 +15,53 @@ sv_ttk.set_theme("light")
 
 connection = sqlite3.connect("inventoryList.db")
 
-# View Audio Inventory WORKING ON
+#Inventory Display Frame
+inventory_frame = ttk.LabelFrame(frame, text="Inventory")
+inventory_frame.grid(row=1, column=0, padx=20, pady=25)
+
+inventory_tree = ttk.Treeview(inventory_frame, columns=("ID", "Name", "Amount"), show="headings", height=10)
+inventory_tree.grid(row=0, column=0, padx=10, pady=10)
+
+inventory_tree.heading("ID", text="ID")
+inventory_tree.heading("Name", text="Name")
+inventory_tree.heading("Amount", text="Amount")
+
+inventory_scrollbar = ttk.Scrollbar(inventory_frame, orient=tkinter.VERTICAL, command=inventory_tree.yview)
+inventory_scrollbar.grid(row=0, column=1, sticky="ns")
+inventory_tree.config(yscrollcommand=inventory_scrollbar.set)
+
+# View Audio Inventory
 def viewAudio():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM Audio")
     data = cursor.fetchall()
-    connection.commit()
-    print(data)
+
+    if view_audio_button.cget("text") == "View Inventory":
+    
+        inventory_frame.grid(row=1, column=0, padx=20, pady=25)
+        view_audio_button.config(text="Close Inventory")
+
+        #Clears existing data from the table
+        inventory_tree.delete(*inventory_tree.get_children())
+
+        for item in data:
+            inventory_tree.insert("", "end", values=item)
+    else:
+        inventory_frame.grid_forget()
+        view_audio_button.config(text="View Inventory")
+
+def hideAudioInventory():
+    inventory_frame.grid_remove()
+
+def refreshInventory():
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Audio")
+    data = cursor.fetchall()
+
+    # Clear existing data from display
+    inventory_tree.delete(*inventory_tree.get_children())
+    for item in data:
+        inventory_tree.insert("", "end", values=item)
 
 # Add Audio Inventory
 def addNewAudio():
@@ -46,18 +86,22 @@ def removeAudio():
     cursor.execute("""DELETE FROM Audio WHERE AudioID = ?""", (del_id,))
     connection.commit()
 
+#Calling the hide function to hide the inventory table when the program is first opened (not sure if this is the best way to do this feel free to change)
+hideAudioInventory()
+
 # Working on this, view inventory function
 view_inv_frame = ttk.LabelFrame(frame, text = "View Inventory")
 view_inv_frame.grid(row = 0, column = 0, padx = 20, pady = 25)
 
-view_audio_button = ttk.Button(view_inv_frame, text = "View Inventory", command = viewAudio)
-view_audio_button.grid(row = 0, column = 0, padx = 15, pady = 15)
+
+view_audio_button = ttk.Button(view_inv_frame, text="View Inventory", command=viewAudio)
+view_audio_button.grid(row=0, column=0, padx=15, pady=15)
 
 #view_audio_button = ttk.Combobox(view_inv_frame, values = ["Audio", "Visual", "Other"])
 #view_audio_button.grid(row = 0, column = 0, padx = 15, pady = 15)
 
 # Add Audio
-inventory_audio_frame = ttk.LabelFrame(frame, text = "Audio Inventory")
+inventory_audio_frame = ttk.LabelFrame(frame, text = "Inventory")
 inventory_audio_frame.grid(row = 2, column = 0, padx = 20, pady = 20)
 
 item_info_frame = ttk.LabelFrame(frame, text = "Edit Inventory")
@@ -90,6 +134,10 @@ edit_audio_button.grid(row = 4, column = 1, sticky = "news", padx = 15, pady = 1
 
 add_audio_button = ttk.Button(item_info_frame, text = "Delete", command = removeAudio)
 add_audio_button.grid(row = 4, column = 2, sticky = "news", padx = 15, pady = 10)
+
+# Refresh button
+refresh_button = ttk.Button(inventory_frame, text="Refresh", command=refreshInventory)
+refresh_button.grid(row=1, column=0, padx=10, pady=5)
 
 window.mainloop()
 
